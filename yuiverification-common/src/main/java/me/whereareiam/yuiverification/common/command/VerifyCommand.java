425c5191457cd72b15ab15570e7a9e941318ebf6
whereareiam <mmm.g032@gmail.com>
2025-12-24 00:50:42 +0100

    fix(commands): replace VerificationMessages with translatable keys in VerifyCommand

--- yuiverification-common/src/main/java/me/whereareiam/yuiverification/common/command/VerifyCommand.java
+++ yuiverification-common/src/main/java/me/whereareiam/yuiverification/common/command/VerifyCommand.java
@@ -9,15 +9,12 @@ import me.whereareiam.yui.model.fluctlight.Fluctlight;
 import me.whereareiam.yui.util.style.StyleKit;
 import me.whereareiam.yui.util.translation.Translatable;
 import me.whereareiam.yuiverification.VerificationService;
-import me.whereareiam.yuiverification.model.config.VerificationMessages;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
 public class VerifyCommand {
 	private final VerificationService verificationService;
-	private final ObjectProvider<VerificationMessages> messages;
 
 	@Definition("verify")
 	@Command("verify <user>")
@@ -26,19 +23,19 @@ public class VerifyCommand {
 			@Argument("user") Fluctlight target
 	) {
 		Fluctlight executor = interaction.fluctlight();
-		VerificationMessages msgs = messages.getObject();
 
 		verificationService.verifyManual(target, executor.getId());
 
-		VerificationMessages.Command.Verify.Success success = msgs.getCommand().getVerify().getSuccess();
-		interaction.replyCallback().replyEmbeds(
-				StyleKit.embeds()
-						.success()
-						.setTitle(success.getTitle())
-						.setDescription(Translatable.text(String.join("\n", success.getDescription()))
-								.with("user", target.getAsMention())
-								.resolve(executor))
-						.build()
-		).setEphemeral(true).queue();
+		interaction.replyCallback()
+				.replyEmbeds(
+						StyleKit.embeds()
+								.success()
+								.setTitle(Translatable.text("plugin.yuiverification.command.verify.success.title").resolve(executor))
+								.setDescription(Translatable.text("plugin.yuiverification.command.verify.success.description")
+										.with("user", target.getAsMention())
+										.resolve(executor))
+								.build()
+				).setEphemeral(true)
+				.queue();
 	}
 }
